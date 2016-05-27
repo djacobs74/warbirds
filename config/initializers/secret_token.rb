ac264330509c9e34886b515768f9b216abaa8b7a
David Jacobs <djacobs74@gmail.com>
2016-05-27 01:23:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Warbirds::Application.config.secret_key_base = '40d0161968c6297e6bb169a5cd2c9d6d6d72c99aced48e75b6cce51d5a301375649630c255ca82bf24fce8ca1b7c7c381118ac9491d28168ffd87d2ab0bd065b'
